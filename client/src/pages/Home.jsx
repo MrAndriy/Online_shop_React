5bb7bfb8ecca8e3b7d01533dc8c6bfaeb59082b0
Andriy Mriy <Andriymriy@gmail.com>
2022-05-09 19:44:19 +0300

    add carousel, navigate to products

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { observer } from 'mobx-react-lite';
+import { Context } from '../index';
+import { fetchDevices } from '../http/productAPI';
+import Slides from '../components/Slides';
 
-const Home = () => {
+const Home = observer(() => {
+	const { products } = useContext(Context);
+
+	useEffect(() => {
+		fetchDevices().then((data) => products.setProducts(data));
+	}, []);
+
 	return (
 		<div>
-			<h1>Home page</h1>
+			<Slides products={products.products} />
 		</div>
 	);
-};
+});
 
 export default Home;
